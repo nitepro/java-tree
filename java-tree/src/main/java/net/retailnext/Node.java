f37d41e3f9fd4dbb2da2c47ab694e4ba21a8aa37
Anirudh Gopalakrishnan <anirudh_gopalakrishnan@intuit.com>
2015-05-04 13:27:00 -0700

    Initial commit

---
+++ java-tree/src/main/java/net/retailnext/Node.java
@@ -0,0 +1,56 @@
+package net.retailnext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Node {
+
+	private String id;
+	private String name;
+	
+	
+	private String attr;
+	private List<Node> children;
+	
+	public Node() {
+		this.children = new ArrayList<Node>();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAttr() {
+		return attr;
+	}
+
+	public void setAttr(String attr) {
+		this.attr = attr;
+	}
+
+	public List<Node> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<Node> children) {
+		this.children = children;
+	}
+	
+	
+	public void addChild(Node n) {
+		this.children.add(n);
+	}
+	
+}
---
+++ java-tree/src/main/java/net/retailnext/Tree.java
@@ -0,0 +1,68 @@
+package net.retailnext;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tree {
+
+	
+	private Node root;
+	
+	
+	public Tree() {
+		//
+	}
+	
+	
+	public void addNode(String id , Node n) throws Exception {
+		
+		Node targetNode = searchNode(this.root, id);
+		
+		if(this.root == null) {
+			this.root = n;
+		}
+		
+		if(targetNode == null) {
+			throw new Exception("Target Node with " + id + " not found");
+		}
+		
+	    targetNode.addChild(n);
+		
+	}
+	
+	
+	private Node searchNode(Node root , String id) {
+		
+		if( root == null) {
+			return null;
+		}
+		
+		
+		List<Node> currentLevel = new ArrayList<Node>();
+		currentLevel.add(root);
+		
+		List<Node> secondLevel = new ArrayList<Node>();
+		
+		
+		while(!currentLevel.isEmpty()) {
+			
+			
+			for(Node n : currentLevel) {
+				if(n.getId().equals(id)) {
+					return n;
+				}
+				secondLevel.addAll(n.getChildren());
+			}
+			
+			currentLevel = secondLevel;
+			
+		}
+		
+		//not found
+		
+		return null;
+		
+		
+	}
+}
+
