817182d8bb6c2ea37d57671a9668d719a6806bb8
Vishal Sadhnani <vishal@retailnext.net>
2015-05-04 11:47:01 -0700

    Maven init

---
+++ java-tree/src/main/java/net/retailnext/App.java
@@ -0,0 +1,13 @@
+package net.retailnext;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
